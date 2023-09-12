3ad07f03937d594c563da147fb691f736e37ecb8
Rg1207 <engfadyibrahim01@gmail.com>
2023-09-12 12:15:50 -0700

    Functions, nested loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,25 @@
 #include "main.h"
 
 /**
- * print_sign - print + if n is greater than zero.
- *              0 is n zero and - if n is less
- *              than zero.
- *
- * @0: takes integer type input for function.
- *
- * Return:1 if +, 0 if 0 and -1 if -  
-*/
-
+ * print_sign - Printys sum
+ * @n: The nmber
+ * Return: 1 for all
+ */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar(43);
 		return (1);
-	} 
-	else if (n == 0)
-	{	
-		_putchar(48);
-		return (0);
-	} 
-	else
+	}
+	else if (n < 0)
 	{
 		_putchar(45);
 		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
 	}
 }
