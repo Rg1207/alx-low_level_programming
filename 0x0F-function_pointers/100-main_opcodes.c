cd9f9da5a1f1c61dcb481ac996a4fa0cea6ebc77
Rg1207 <engfadyibrahim01@gmail.com>
2023-10-11 07:35:26 -0700

    Functions Pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,25 +1,42 @@
-#include "function_pointers.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
- * main - check the code for the school students
+ * main - points its own opcodes
  * @argc: the number of args
- * @argv: argument vector
+ * @argv: array of arguments
  *
- * Return: Alwas 0.
+ * Return: Alwas 0. (Success)
  */
-int main(int argc, char **argv)
+int main(int argc, char **argiv)
 {
-	char *p = (char *)main;
-	int b;
+	int bytes, i;
+	char *arr;
 
-	if (argc !=2)
-		printf("Error\n"), exit(1);
-	b = atoi(argv[1]);
-	if (b < 0)
-		printf("Error\n"), exit(2);
-	while (b--)
-		printf("%2hhx%s", *p++, b ? " " : "\n");
-	return (0)
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
 }
