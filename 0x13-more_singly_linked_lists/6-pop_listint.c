0f1a29272a1b9ede099560ec366be7af5326c2a9
Rg1207 <engfadyibrahim01@gmail.com>
2023-10-24 07:42:41 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,10 @@
 #include "lists.h"
-
 /**
- * pop_listint - pops head node of list 
+ * pop_listint - pops head node of list
  * @head: address of pointer to first node
  *
  * Return: value of popped node
  */
-  
 int pop_listint(listint_t **head)
 {
 	listint_t *node;
