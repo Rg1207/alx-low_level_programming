ce997c94c08ca1273139b42ab0dee6ddb291f3ed
Rg1207 <engfadyibrahim01@gmail.com>
2023-10-30 05:59:31 -0700

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -36,6 +36,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (len)
 		bytes = write(fd, text_content, len);
-	clode(fd);
+	close(fd);
 	return (bytes == len ? 1 : -1);
 }
