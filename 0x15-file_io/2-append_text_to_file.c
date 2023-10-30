35a029e4ab2367222cb6ad1278919ddeed140b6b
Rg1207 <engfadyibrahim01@gmail.com>
2023-10-30 07:00:14 -0700

    File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -6,36 +6,41 @@
  *
  * Return: integer length of string
  */
-int _strlen(char *s)
+int _strlen(const char *s)
 {
-	int i = 0;
+int i = 0;
 
-	if (!s)
-		return (0);
+if (!s)
+return (0);
 
-	while (*s++)
-		i++;
-	return (i);
+while (*s++)
+i++;
+
+return (i);
 }
+
 /**
  * append_text_to_file - appends text to a file
  * @filename: name of file to create
  * @text_content: text to write
  *
- * Return: 1 on success 0 on failure
+ * Return: 1 on success, -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	ssize_t bytes = 0, len = _strlen(text_content);
+int fd;
+ssize_t bytes = 0, len = _strlen(text_content);
 
-	if (!filename)
-		return (-1);
-	fd = open(filename, O_APPEND);
-	if (fd == -1 )
-		return (-1);
-	if (len)
-		bytes = write(fd, text_content, len);
-	close(fd);
-	return (bytes == len ? 1 : -1);
+if (!filename)
+return (-1);
+
+fd = open(filename, O_APPEND);
+if (fd == -1)
+return (-1);
+
+if (len)
+bytes = write(fd, text_content, len);
+
+close(fd);
+return (bytes == len ? 1 : -1);
 }
