31031b1e8223083a7411afb4e74522e4a1a4b1f5
Rg1207 <engfadyibrahim01@gmail.com>
2023-10-30 09:07:23 -0700

    File I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,18 +6,11 @@
 #define ERR_NOCLOSE "ERROR: Can't close fd %d\n"
 #define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
-/**
- * main - program
- * @ac: argument count
- * @av: argument vector
- *
- * Return: 1 on success, 0 on failure
- */
 int main(int ac, char **av)
 {
-int from_fd = 0, to_fd = 0;
-ssize_t b;
-char buf[READ_BUF_SIZE];
+	int from_fd = 0, to_fd = 0;
+	ssize_t b;
+	char buf[READ_BUF_SIZE];
 
 if (ac != 3)
 dprintf(STDERR_FILENO, USAGE), exit(97);
@@ -27,16 +20,19 @@ dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 to_fd = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
 if (to_fd == -1)
 dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
 while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
 if (write(to_fd, buf, b) != b)
 dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
+
 if (b == -1)
 dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
 
 if (close(from_fd) == -1)
 dprintf(STDERR_FILENO, ERR_NOCLOSE, from_fd), exit(100);
+
 if (close(to_fd) == -1)
-dprintf(STDERR_FILENO, ERR_NOCLOSE, to_fd), exit(100);
+	    dprintf(STDERR_FILENO, ERR_NOCLOSE, to_fd), exit(100);
 
 return (EXIT_SUCCESS);
 }
